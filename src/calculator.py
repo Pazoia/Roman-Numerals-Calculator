169377a7f22794bcaab8065ba27980bf43926eac
Paulo Azoia <pazoia.gomes@gmail.com>
2022-01-12 13:49:40 +0000

    refactored calculator functions to work with refactored get_key function

--- src/calculator.py
+++ src/calculator.py
@@ -1,22 +1,22 @@
 from .roman_numerals import roman_numerals
-from .roman_numerals import get_key
+from .helper_functions import get_key
 
 class Calculator:
     
     def add(roman_num_1, roman_num_2):
         result = roman_numerals[roman_num_1] + roman_numerals[roman_num_2]
-        return get_key(result)
+        return get_key(result, roman_numerals)
 
     def multiply(roman_num_1, roman_num_2):
         result = roman_numerals[roman_num_1] * roman_numerals[roman_num_2]
-        return get_key(result)
+        return get_key(result, roman_numerals)
 
     def subtract(roman_num_1, roman_num_2):
         result = roman_numerals[roman_num_1] - roman_numerals[roman_num_2]
-        return get_key(result)
+        return get_key(result, roman_numerals)
 
     def divide(roman_num_1, roman_num_2):
         result = roman_numerals[roman_num_1] / roman_numerals[roman_num_2]
-        return get_key(result)
+        return get_key(result, roman_numerals)
 
     
