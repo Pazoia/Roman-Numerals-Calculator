eff6f7efc67659c921ee913fe2a970eac18cc9bc
Paulo Azoia <pazoia.gomes@gmail.com>
2022-01-12 03:04:18 +0000

    multiplication of two roman numerals, test passing

--- src/calculator.py
+++ src/calculator.py
@@ -3,8 +3,12 @@ from .roman_numerals import get_key
 
 class Calculator:
     
-    def addition(roman_num_1, roman_num_2):
+    def add(roman_num_1, roman_num_2):
         result = roman_numerals[roman_num_1] + roman_numerals[roman_num_2]
-        print(result)
         return get_key(result)
+
+    def multiply(roman_num_1, roman_num_2):
+        result = roman_numerals[roman_num_1] * roman_numerals[roman_num_2]
+        return get_key(result)
+
     
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,4 +1,7 @@
 from src.calculator import Calculator
 
 def test_addition_of_two_roman_numerals():
-    assert Calculator.addition("I", "I") == "II"
+    assert Calculator.add("I", "I") == "II"
+
+def test_multiplication_of_two_roman_numerals():
+    assert Calculator.multiply("II", "V") == "X"
