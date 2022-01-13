3e608e032902c89b9c43942cf4ac83f7d7604fd3
Paulo Azoia <pazoia.gomes@gmail.com>
2022-01-13 21:47:19 +0000

    added numbers 40, 90, 400 and 900 to both dictionaries

--- src/roman_numerals.py
+++ src/roman_numerals.py
@@ -9,9 +9,13 @@ roman_numerals_to_integer = {
     "VIII": 8,
     "IX": 9,
     "X": 10,
+    "XL": 40,
     "L": 50,
+    "XC": 90,
     "C": 100,
+    "CD": 400,
     "D": 500,
+    "DM": 900,
     "M": 1000,
 }
 
@@ -26,8 +30,12 @@ integer_to_roman_numerals = {
     8: "VIII",
     9: "IX",
     10: "X",
+    40: "XL",
     50: "L",
+    90: "XC",
     100: "C",
+    400: "CD",
     500: "D",
+    900: "DM",
     1000: "M",
 }
