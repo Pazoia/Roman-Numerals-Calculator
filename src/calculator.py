c814bf2024d11af940a3b400237f3a3e0045c3df
Paulo Azoia <pazoia.gomes@gmail.com>
2022-01-13 10:41:53 +0000

    updated calculator function to use refactored dicts in roman numerals module

--- src/calculator.py
+++ src/calculator.py
@@ -1,22 +1,22 @@
-from .roman_numerals import roman_numerals
-from .helper_functions import get_key
+from .roman_numerals import roman_numerals_to_integer
+from .roman_numerals import integer_to_roman_numerals
 
 class Calculator:
     
     def add(roman_num_1, roman_num_2):
-        result = roman_numerals[roman_num_1] + roman_numerals[roman_num_2]
-        return get_key(result, roman_numerals)
+        result = roman_numerals_to_integer[roman_num_1] + roman_numerals_to_integer[roman_num_2]
+        return integer_to_roman_numerals[result]
 
     def multiply(roman_num_1, roman_num_2):
-        result = roman_numerals[roman_num_1] * roman_numerals[roman_num_2]
-        return get_key(result, roman_numerals)
+        result = roman_numerals_to_integer[roman_num_1] * roman_numerals_to_integer[roman_num_2]
+        return integer_to_roman_numerals[result]
 
     def subtract(roman_num_1, roman_num_2):
-        result = roman_numerals[roman_num_1] - roman_numerals[roman_num_2]
-        return get_key(result, roman_numerals)
+        result = roman_numerals_to_integer[roman_num_1] - roman_numerals_to_integer[roman_num_2]
+        return integer_to_roman_numerals[result]
 
     def divide(roman_num_1, roman_num_2):
-        result = roman_numerals[roman_num_1] / roman_numerals[roman_num_2]
-        return get_key(result, roman_numerals)
+        result = roman_numerals_to_integer[roman_num_1] / roman_numerals_to_integer[roman_num_2]
+        return integer_to_roman_numerals[result]
 
     
