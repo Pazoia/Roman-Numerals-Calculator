f2beaa64e229ffb602d8d40f3438b41e857e4c2c
Paulo Azoia <pazoia.gomes@gmail.com>
2022-01-15 18:45:30 +0000

    remaned files to reflect better on their functionality:
    Added class Converter to file roman_numeral_converter,
    renamed function to convert_roman_num_to_int to better describe it's functionality

--- src/roman_numeral_converter.py
+++ src/roman_numeral_converter.py
@@ -1,16 +1,15 @@
 from src.roman_numerals import roman_numerals_to_integer
 from functools import reduce
 
-def roman_num_validator(roman_num):
+class Converter:
+    def __init__(self):
+        self.values = []
 
-    if roman_num in roman_numerals_to_integer:
-        return roman_numerals_to_integer[roman_num]
-
-    if roman_num not in roman_numerals_to_integer:
+    def convert_roman_num_to_int(self, roman_num):
+        
         def split(word):
             return list(word)
 
-        values = []
         roman_num_breakdown = split(roman_num)
         
         for i in range(len(roman_num_breakdown)):
@@ -18,24 +17,24 @@ def roman_num_validator(roman_num):
                 break
 
             if len(roman_num_breakdown) == 1:
-                values.append(roman_numerals_to_integer[roman_num_breakdown[0]])
+                self.values.append(roman_numerals_to_integer[roman_num_breakdown[0]])
                 roman_num_breakdown.remove(roman_num_breakdown[0])
                 break
 
             paired_chars = f"{roman_num_breakdown[0]}{roman_num_breakdown[1]}"
             
             if paired_chars not in roman_numerals_to_integer:
-                values.append(roman_numerals_to_integer[roman_num_breakdown[0]])
+                self.values.append(roman_numerals_to_integer[roman_num_breakdown[0]])
                 roman_num_breakdown.remove(roman_num_breakdown[0])
 
             if paired_chars in roman_numerals_to_integer and len(roman_num_breakdown) <= 2:
-                values.append(roman_numerals_to_integer[paired_chars])
+                self.values.append(roman_numerals_to_integer[paired_chars])
                 roman_num_breakdown.remove(roman_num_breakdown[0])
                 roman_num_breakdown.remove(roman_num_breakdown[0])
                 break
             elif paired_chars in roman_numerals_to_integer:
-                values.append(roman_numerals_to_integer[paired_chars])
+                self.values.append(roman_numerals_to_integer[paired_chars])
                 roman_num_breakdown.remove(roman_num_breakdown[0])
                 roman_num_breakdown.remove(roman_num_breakdown[0])
 
-        return reduce(lambda x, y: x+y, values)
+        return reduce(lambda x, y: x+y, self.values)
--- tests/roman_num_converter_test.py
+++ tests/roman_num_converter_test.py
@@ -1,4 +1,5 @@
-from src.input_roman_numeral_validator import roman_num_validator
+from lib2to3.pytree import convert
+from src.roman_numeral_converter import Converter
 import pytest
 
 @pytest.mark.parametrize("roman_num, expected_integer", [
@@ -10,5 +11,6 @@ import pytest
     pytest.param("XCVIII", 98, id="when_not_in_roman_numerals_to_integer_and_first_number_of_the_result_is_9"),
 ])
 
-def test_returns_correct_integer(roman_num, expected_integer):
-    assert roman_num_validator(roman_num) == expected_integer
+def test_returns_correct_value(roman_num, expected_integer):
+    converter = Converter()
+    assert converter.convert_roman_num_to_int(roman_num) == expected_integer
